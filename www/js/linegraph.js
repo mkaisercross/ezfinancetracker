f357f5b34e83430da74a42364e9df95c7408878d
kdubuis1 <kdubuis1@fau.edu>
2015-04-23 05:32:29 -0400

    added graph details and values

--- www/js/linegraph.js
+++ www/js/linegraph.js
@@ -1,14 +1,14 @@
 // Get the context of the canvas element we want to select
 
 var buyerData = {
-  labels : ["January","February","March","April","May","June"],
+  labels : ["January","February","March","April","May","June","July"],
   datasets : [
     {
       fillColor : "rgba(172,194,132,0.4)",
       strokeColor : "#ACC26D",
       pointColor : "#fff",
       pointStrokeColor : "#9DB86D",
-      data : [203,156,99,251,305,247]
+      data : [625.43, 659.23, 770.41, 681.64, 536.51, 755.13, 433.63]
     }
   ]
 }
--- www/js/piegraph.js
+++ www/js/piegraph.js
@@ -1,21 +1,33 @@
 var data = [
     {
-        value: 300,
+        value: 35.72,
         color:"#F7464A",
         highlight: "#FF5A5E",
-        label: "Red"
+        label: "Chipotle"
     },
     {
-        value: 50,
+        value: 123.43,
         color: "#46BFBD",
         highlight: "#5AD3D1",
-        label: "Green"
+        label: "WholeFoods"
     },
     {
-        value: 100,
+        value: 26.96,
         color: "#FDB45C",
         highlight: "#FFC870",
-        label: "Yellow"
+        label: "McDonalds"
+    },
+    {
+        value: 23.15,
+        color: "#19A347",
+        highlight: "#33AD5C",
+        label: "Wawa"
+    },
+    {
+        value: 54.41,
+        color: "#FF75D1",
+        highlight: "#FF94DB",
+        label: "Sushi Hut"
     }
 ];
 var buyers1 = document.getElementById("buyers1").getContext('2d');
