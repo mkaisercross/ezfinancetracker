2b63ab9630c9b3ecbbc364f2786bf4ed22d706f2
Michael Kaiser-Cross <mkaisercross@gmail.com>
2015-05-03 00:12:43 -0400

    add function to change dom from precapture to postcapture. ie editing ocr results

--- www/js/takephoto.js
+++ www/js/takephoto.js
@@ -1,6 +1,8 @@
 
 
 
+    $('#loading').css("display", "none");
+
     var pictureSource;   // picture source
     var destinationType; // sets the format of returned value
 
@@ -10,17 +12,71 @@
 
     // device APIs are available
     //
+
+    $(".content").on("click", "#capture-photo", function() {
+        capturePhoto();        
+        $('#choose-capture-type').css("display", "none");
+
+    });
+
     function onDeviceReady() {
         pictureSource=navigator.camera.PictureSourceType;
         destinationType=navigator.camera.DestinationType;
     }
+
+
+
+    function getEditModeHtml() {
+      var editModeHtml = 
+	'<form id="login" class="pure-form pure-form-stacked login" action="splash.html" method="pre">' +
+	  '<fieldset>' +
+	    '<label for="store_name">Store Name</label>' +
+	    '<input class="store_name" type="text" name="" required value="" >' +
+	    '<label for="purchase_date">Purchase Date</label>' +
+	    '<input class="purchase_date" type="text" name="" required value="" >' +
+	    '<style>' +
+		'.editModeItem { margin-left: 10px; float: left; clear: right;}' +
+		'.editModeItem label { float: left; clear: left;}' +
+		'.editModeItem input { float: left; }' +
+	    '</style>' +
+	    '<label for="">Item </label>' +
+	    '<div class="editModeItem" style="margin-left: 20px;">' +
+	      '<table class="pure-table pure-table-horizontal">' +
+      	         '<tbody>' +
+		  '<tr>' +
+		    '<td><label for="item-name">Name</label></td>' +
+		    '<td><input class="item-name" type="text" name="" required value="" ></td>' +
+		  '</tr>' +
+		  '<tr>' +
+		    '<td><label for="item-price">Price</label></td>' +
+		    '<td><input class="item-price" type="text" name="" required value="" ></td>' +
+		  '</tr>' +
+		  '<tr>' +
+		    '<td><label for="item-category">Category</label></td>' +
+		    '<td><input class="item-category" type="text" name="" required value="" ></td>' +
+		  '</tr>' +
+		'</tbody>' +
+	      '</table>' +
+	    '</div>' +
+	    '<label for="">Subtotal</label>' +
+	    '<input class="subtotal" type="text" name="" required value="" >' +
+	    '<label for="">Tax</label>' +
+	    '<input class="tax" type="text" name="" required value="" >' +
+	    '<label for="">Total</label>' +
+	    '<input class="total" type="text" name="" required value="" >' +
+	    '<button id="Login" class="pure-button pure-button-primary float-left submit-button" >Login</button>' +
+	  '</fieldset>' +
+	'</form>';
+      return editModeHtml
+    }
+
 
     // Called when a photo is successfully retrieved
     //
     function onPhotoDataSuccess(imageData) {
       // Uncomment to view the base64-encoded image data
       //console.log(imageData);
-
+      $('#loading').css("display", "block");
       // Get image handle
       //
       var smallImage = document.getElementById('smallImage');
@@ -51,8 +107,9 @@
       var myRequest = new XMLHttpRequest();
       myRequest.open("POST", "http://45.55.160.70/upload_image.php", false);
       myRequest.send(imageData);
-      /*$("body").append("<p> upload successful </p>");*/
-      $("body").html(myRequest.responseText);      
+      $('#loading').css("display", "none");
+      $(".content").append($.parseHTML(getEditModeHtml()));
+      $(".content").append($.parseHTML(myRequest.responseText));
       /*
       var imgData = JSON.stringify(imageData);
       $.ajax({
@@ -87,6 +144,7 @@
       //
       largeImage.src = imageURI;
 
+
     }
 
     // A button will call this function
@@ -95,6 +153,7 @@
       // Take picture using device camera and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
         destinationType: destinationType.DATA_URL });
+
     }
 
     // A button will call this function
@@ -112,6 +171,7 @@
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
         destinationType: destinationType.FILE_URI,
         sourceType: source });
+
     }
 	
 	 
