a2ee30892c150028057914644823421a3c83b0fa
Michael Kaiser-Cross <mkaisercross@gmail.com>
2015-05-04 09:21:05 -0400

    initial commit for restructure-as-angular branch

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+var ezFinanceTrackerControllers = angular.module('ezFinanceTrackerControllers', []);
+
+ezFinanceTrackerControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
+
+ezFinanceTrackerControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
